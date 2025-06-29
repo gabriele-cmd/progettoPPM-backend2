bb527e9cb45aa3c3d42eea5acc97daad8aa99b42
gabriele-cmd <johndoe@example.com>
2025-06-29 17:23:16 +0200

    modificati permessi moderatore

--- users/admin.py
+++ users/admin.py
@@ -24,5 +24,27 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
 
-admin.site.register(CustomUser, CustomUserAdmin)
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return []
 
+        # Se è staff, ma non superuser
+        if request.user.is_staff:
+            readonly = [
+                'password',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+                'last_login',
+                'date_joined',
+            ]
+            return readonly
+
+        return super().get_readonly_fields(request, obj)
+
+    def get_fieldsets(self, request, obj=None):
+        return super().get_fieldsets(request, obj)
+
+#admin.site.unregister(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
