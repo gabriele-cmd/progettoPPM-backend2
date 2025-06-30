eb1f1bf9e4900208898eb6c3ba290e0b12abd94f
gabriele-cmd <johndoe@example.com>
2025-06-30 15:24:31 +0200

    corretto comando collectstatic --noinput

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py tailwind install
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 python manage.py tailwind start
